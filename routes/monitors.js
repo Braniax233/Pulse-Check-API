cb51420af4723c12200bbe109a0ed953a20f6274
braniax233 <braniax123@gmail.com>
2026-04-11 17:55:44 +0000

    feat: implement RESTful routes for device monitoring

--- routes/monitors.js
+++ routes/monitors.js
@@ -5,6 +5,6 @@ const ctrl = require('../controllers/monitorController');
 router.post('/', ctrl.registerMonitor);
 router.post('/:id/heartbeat', ctrl.heartbeat);
 router.post('/:id/pause', ctrl.pauseMonitor);
-router.get('/', ctrl.listMonitors); // Developer's Choice
+router.get('/', ctrl.listMonitors); //My Developer's Choice
 
 module.exports = router;
